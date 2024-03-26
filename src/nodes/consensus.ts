15b6df9537375b96a44d4b7284cd7fc8d22c009b
Zeitrom <hugo.verley@gmail.com>
2024-03-26 16:59:06 +0100

    fix nodes launch

--- src/nodes/consensus.ts
+++ src/nodes/consensus.ts
@@ -1,50 +1,15 @@
 import { BASE_NODE_PORT } from "../config";
-import http from "http";
 
 export async function startConsensus(N: number) {
-  // Launch a node
+  // launch a node
   for (let index = 0; index < N; index++) {
-    const options = {
-      hostname: "localhost",
-      port: BASE_NODE_PORT + index,
-      path: "/start",
-      method: "GET",
-    };
-
-    const req = http.request(options, (res) => {
-      if (res.statusCode !== 200) {
-        console.error(`Failed to start consensus on node ${index}. Status code: ${res.statusCode}`);
-      }
-    });
-
-    req.on("error", (error) => {
-      console.error(`Failed to start consensus on node ${index}:`, error);
-    });
-
-    req.end();
+    await fetch(`http://localhost:${BASE_NODE_PORT + index}/start`);
   }
 }
 
 export async function stopConsensus(N: number) {
-  // Stop consensus on each node
+  // launch a node
   for (let index = 0; index < N; index++) {
-    const options = {
-      hostname: "localhost",
-      port: BASE_NODE_PORT + index,
-      path: "/stop",
-      method: "GET",
-    };
-
-    const req = http.request(options, (res) => {
-      if (res.statusCode !== 200) {
-        console.error(`Failed to stop consensus on node ${index}. Status code: ${res.statusCode}`);
-      }
-    });
-
-    req.on("error", (error) => {
-      console.error(`Failed to stop consensus on node ${index}:`, error);
-    });
-
-    req.end();
+    await fetch(`http://localhost:${BASE_NODE_PORT + index}/stop`);
   }
 }
