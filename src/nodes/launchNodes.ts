c0d2c9cd632820b43048e9076a487082005b347a
Zeitrom <hugo.verley@gmail.com>
2024-03-26 17:00:19 +0100

    updates launch

--- src/nodes/launchNodes.ts
+++ src/nodes/launchNodes.ts
@@ -2,28 +2,29 @@ import { Value } from "../types";
 import { node } from "./node";
 
 export async function launchNodes(
-  N: number,
-  F: number,
-  initialValues: Value[],
-  faultyList: boolean[]
+  N: number, // total number of nodes in the network
+  F: number, // number of faulty nodes in the network
+  initialValues: Value[], // initial values of each node
+  faultyList: boolean[] // list of faulty values for each node, true if the node is faulty, false otherwise
 ) {
   if (initialValues.length !== faultyList.length || N !== initialValues.length)
     throw new Error("Arrays don't match");
   if (faultyList.filter((el) => el === true).length !== F)
-    throw new Error("faultyList doesn't have F faulties");
+    throw new Error("faultyList doesnt have F faulties");
 
-  const promises: Promise<any>[] = [];
-  const nodesStates: boolean[] = new Array(N).fill(false);
+  const promises = [];
+
+  const nodesStates = new Array(N).fill(false);
 
   function nodesAreReady() {
-    return nodesStates.every((el) => el === true);
+    return nodesStates.find((el) => el === false) === undefined;
   }
 
   function setNodeIsReady(index: number) {
     nodesStates[index] = true;
   }
 
-  // Launch nodes
+  // launch nodes
   for (let index = 0; index < N; index++) {
     const newPromise = node(
       index,
